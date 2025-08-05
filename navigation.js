7319a8fc5d96307118b1a7fc2ebd1605c23e4d02
TirthaPM <07tirtha@gmail.com>
2025-08-05 11:38:22 +0600

    Delete navigation.js

--- navigation.js
+++
@@ -1,9 +0,0 @@
-// Accessible Navigation
-const tabButtons = document.querySelectorAll('.tab-btn');
-tabButtons.forEach(btn => {
-  btn.setAttribute('tabindex', '0');
-  btn.setAttribute('role', 'button');
-  btn.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter' || e.key === ' ') btn.click();
-  });
-});
